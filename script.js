56c03f3502366ef6dab0d44701b35efcc723adb6
Fatih SOYSAL : Sessiz Çığlık'larla Dâhi - Ezber YAZAN - YALNIZ Bir RÜZGAR <106076235+fsoysall@users.noreply.github.com>
2022-09-22 04:10:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,25 +7,32 @@ const logH = (s) => { log.innerHTML += s; }
 
 logH(`<hr><h3>LocalStorage</h3>`);
 
-var keyLS = "keyLS-in-valuesi";
-localStorage.setItem(keyLS.toString(), keyLS); // keyLS değişkeninin içeriğini localStorage'e YAZ
+var keyLS = "keyLS"
+var keyLSVal = `keyLS-in-valuesi ${new Date().toTimeString()}`;
+
+localStorage.setItem(keyLS, keyLSVal); // keyLS değişkeninin içeriğini localStorage'e YAZ
 logH(`<br>LS'ye Kaydedildi`);
 
-var keyLSReaded = localStorage.getItem("keyLS"); // key değişkeninin içeriğini localStorage'den al
-logH(`<br>LS'den OKUNAN : [ ${keyLS} ]`);
-localStorage.removeItem("keyLS"); // key değişkeninin içeriğini localStorage'den al
-logH(`<br>LS'den Silindi`);
+var keyLSReaded = localStorage.getItem(keyLS); // key değişkeninin içeriğini localStorage'den al
+logH(`<br>LS'den OKUNAN : [ ${keyLSReaded} ]`);
 
+// localStorage.removeItem( keyLS ); // key değişkeninin içeriğini localStorage'den SİL
+// logH(`<br>LS'den Silindi`);
 
 if (sessionStorage) {
-    logH(`<hr><h3>LocaSStorage</h3>`);
 
-    var keySS = "keySS-in-valuesi";
-    sessionStorage.setItem(keySS.toString(), keySS); // keySS değişkeninin içeriğini sessionStorage'e YAZ
+    logH(`<hr><h3>sessionStorage</h3>`);
+    
+    var keySS = "keySS"
+    var keySSVal = `keySS-in-valuesi ${new Date().toTimeString()}`;
+
+    sessionStorage.setItem(keySS, keySSVal); // keySS değişkeninin içeriğini sessionStorage'e YAZ
     logH(`<br>SS'ye Kaydedildi`);
 
-    var keySSReaded = sessionStorage.getItem("keySS"); // key değişkeninin içeriğini sessionStorage'den al
-    logH(`<br>SS'den OKUNAN : [ ${keySS} ]`);
-    sessionStorage.removeItem("keySS"); // key değişkeninin içeriğini sessionStorage'den al
-    logH(`<br>SS'den Silindi`);
+    var keySSReaded = sessionStorage.getItem(keySS); // key değişkeninin içeriğini sessionStorage'den al
+    logH(`<br>SS'den OKUNAN : [ ${keySSReaded} ]`);
+
+    // sessionStorage.removeItem( keySS ); // key değişkeninin içeriğini sessionStorage'den SİL
+    // logH(`<br>SS'den Silindi`);
+
 }
